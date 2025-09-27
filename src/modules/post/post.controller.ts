f4cf1dc6dacaf0e0f11a2187af1acc2e45cf7c26
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-09-27 20:55:48 +0600

    prisma-transaction-api

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -16,7 +16,15 @@ const getAllPost = async(req: Request, res: Response) =>{
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
         const search = (req.query.search as string) || "" ;
-        const result = await PostService.getAllPost({page, limit, search});
+        const isFeatured = req.query.isFeatured ? req.query.isFeatured === "true" : undefined;
+        const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+        const sortByTitle = (req.query.title as string) || "";
+        const orderBy = (req.query.orderBy as string) || "";
+
+
+        const result = await PostService.getAllPost({page, limit, search, isFeatured, tags, sortByTitle, orderBy});
+        
+
         res.status(200).send(result)
     } catch (error) {
         res.status(500).send(error)
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,7 +1,7 @@
 import { Post, Prisma } from "@prisma/client"
 import { prisma } from "../../config/db"
 
-const createPost = async(payload: Prisma.PostCreateInput): Promise<Post> =>{
+const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
     const result = await prisma.post.create({
         data: payload,
         include: {
@@ -19,49 +19,100 @@ const createPost = async(payload: Prisma.PostCreateInput): Promise<Post> =>{
 
 
 
-const getAllPost = async ({page, limit, search} : {page: number, limit: number, search: string}) =>{
+const getAllPost = async ({ page = 1, limit = 10, search, isFeatured, tags, sortByTitle, orderBy }: { page?: number, limit?: number, search?: string, isFeatured?: boolean, tags?: string[], sortByTitle?: string, orderBy?: string }) => {
+
+
+    const where: any = {
+        AND: [
+            search && {
+                OR: [
+                    {
+                        title: {
+                            contains: search,
+                            mode: 'insensitive'
+                        }
+                    },
+                    {
+                        content: {
+                            contains: search,
+                            mode: 'insensitive'
+                        }
+                    }
+                ]
+            },
+            typeof isFeatured === 'boolean' && { isFeatured },
+            (tags && tags.length > 0) && { tags: { hasEvery: tags } }
+
+        ].filter(Boolean)
+    }
 
     // console.log({page, limit});
-    const skip = (page -1) * limit;
+    // console.log(isFeatured);
+    // console.log({tags});
+
+    const skip = (page - 1) * limit;
+
+    let orderByClause: any = { createdAt: "desc" }; // default
+    if (sortByTitle && orderBy) {
+        orderByClause = {
+            [sortByTitle]: orderBy.toLowerCase() === "asc" ? "asc" : "desc",
+        };
+    }
+
     const result = await prisma.post.findMany({
         skip,
         take: limit,
-        where: {
-            OR: [
-                {
-                    title: {
-                        contains: search,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    content: {
-                        contains: search,
-                        mode: 'insensitive'
-                    }
-                }
-            ]
-        }
-    })
-    return result;
-}
+        where,
+        orderBy: orderByClause
 
-
-const getPostById = async(id: number) =>{
-    const result = await prisma.post.findUnique({
-        where: {
-            id
-        },
-        include: {
-            author: true
-        }
     });
-    return result;
+
+
+    const total = await prisma.post.count({ where })
+
+    // return result;
+    return {
+        data: result,
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit)
+    };
+}
+
+
+const getPostById = async (id: number) => {
+
+    return await prisma.$transaction(async (tx) => {
+        const updateViewCount = await tx.post.update({
+            where: { id },
+            data: {
+                view: {
+                    increment: 1
+                }
+            }
+        });
+
+
+        const result = await tx.post.findUnique({
+            where: {
+                id
+            },
+            include: {
+                author: true
+            }
+        });
+
+        return result;
+    })
+
+
+
 }
 
 
 
-const updatePost = async(id: number, payload: Partial<Post>) =>{
+const updatePost = async (id: number, payload: Partial<Post>) => {
     const result = await prisma.post.update({
         where: {
             id
@@ -73,7 +124,7 @@ const updatePost = async(id: number, payload: Partial<Post>) =>{
 
 
 
-const deletePost = async(id: number) =>{
+const deletePost = async (id: number) => {
     const result = await prisma.post.delete({
         where: {
             id
